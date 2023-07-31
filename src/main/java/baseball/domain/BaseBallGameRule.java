ddad7eba5388fbaa73167c59b58901114397f161
chosunghyun18 <krismon@naver.com>
2023-07-31 18:19:30 +0900

    feat : add game - 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다. - 게임을 진행 한다.

--- src/main/java/baseball/domain/BaseBallGameRule.java
+++ src/main/java/baseball/domain/BaseBallGameRule.java
@@ -2,8 +2,13 @@ package baseball.domain;
 
 abstract public class BaseBallGameRule {
 
+    protected static final String GAME_NAME = "숫자 야구";
     protected static int maxNumberOfBallsAmount = 3;
     protected int minValueOfBall = 1;
     protected int maxValueOfBall = 9;
-
+    protected static int restartGameValue = 1;
+    protected static int endGameValue = 2;
+    protected int strikeHit = 1;
+    protected int ballHit = 2;
+    protected int nothingHit = 0;
 }
