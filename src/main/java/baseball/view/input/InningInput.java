9292e4e6df520eab99bd6e3b0d908386cd34c099
Brian <bwook9908@gmail.com>
2023-07-31 14:22:50 +0900

    feat: 입력 로직 작성

--- src/main/java/baseball/view/input/InningInput.java
+++ src/main/java/baseball/view/input/InningInput.java
@@ -1,4 +1,20 @@
 package baseball.view.input;
 
+import baseball.util.Convertion;
+import baseball.view.output.Notification;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class InningInput {
+    private ArrayList<Integer> numberInput() {
+        Notification.numberInput();
+        String input = Console.readLine();
+        return Convertion.stringToArrayList(input);
+    }
+    private int restartInput() {
+        Notification.gameRestart();
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
 }
--- src/main/java/baseball/view/output/Notification.java
+++ src/main/java/baseball/view/output/Notification.java
@@ -1,15 +1,19 @@
 package baseball.view.output;
 
 public class Notification {
-    private void gameStart() {
+    public static void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    private void gameEnd() {
+    public static void gameEnd() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    private void gameRestart() {
+    public static void gameRestart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+    public static void numberInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
 }
