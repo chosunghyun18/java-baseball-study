15b298eeec6a276794db961e9454eb56b4174d79
chosunghyun18 <krismon@naver.com>
2023-08-01 22:13:02 +0900

    refact : add static in baseball game field

--- src/main/java/baseball/domain/BaseBallGameRule.java
+++ src/main/java/baseball/domain/BaseBallGameRule.java
@@ -3,12 +3,15 @@ package baseball.domain;
 abstract public class BaseBallGameRule {
 
     protected static final String GAME_NAME = "숫자 야구";
-    protected static int maxNumberOfBallsAmount = 3;
-    protected int minValueOfBall = 1;
-    protected int maxValueOfBall = 9;
-    protected static int restartGameValue = 1;
-    protected static int endGameValue = 2;
-    protected int strikeHit = 1;
-    protected int ballHit = 2;
-    protected int nothingHit = 0;
+    protected static final int maxNumberOfBallsAmount = 3;
+    protected static final int minValueOfBall = 1;
+    protected static final int maxValueOfBall = 9;
+    protected static final int restartGameValue = 1;
+    protected static final int endGameValue = 2;
+    protected static final int strikeHit = 1;
+    protected static final int ballHit = 2;
+    protected final int nothingHit = 0;
+    protected BaseBallGameRule() {
+
+    }
 }
