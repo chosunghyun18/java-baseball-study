b1dda55095a9f634568b1817aa1d8b14b024a114
seonghwa95 <holyflower95@naver.com>
2024-01-12 23:44:03 +0900

    feat:abstract class, member feild

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 상대방(컴퓨터) 값 입력받기
+        List<Integer> computerValue = new ArrayList<>();
+        while (computerValue.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!computerValue.contains(randomNum)) {
+                computerValue.add(randomNum);
+            }
+        }
+
+        Game game = new BaseballGame(computerValue);
+        game.gameStart();
     }
 }
