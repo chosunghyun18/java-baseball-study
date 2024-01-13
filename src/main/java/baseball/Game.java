0e204a58c06045c1b258cf54370dcd48cf80d7a8
seonghwa95 <holyflower95@naver.com>
2024-01-13 23:48:21 +0900

    feat: 입력받기 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public abstract class Game {
@@ -11,5 +13,17 @@ public abstract class Game {
 
     public void gameStart() {
         System.out.println("게임을 시작합니다.");
+    }
+
+    public void inputNum() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
+        String[] splitInput = input.split("");
+        Integer[] inputArray = new Integer[3];
+
+        for (int i = 0; i < 3; i++) {
+            inputArray[i] = Integer.parseInt(splitInput[i]);
+        }
     }
 }
